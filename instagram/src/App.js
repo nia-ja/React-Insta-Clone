73826a1fdfc4b83f3145775e954ed38bf6f3a4bf
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-12 19:43:55 -0700

    Initial commit

---
+++ instagram/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hhh
+      </div>
+    );
+  }
+}
+
+export default App;
