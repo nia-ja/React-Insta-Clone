92b37e1e0ef0713a35590dad3b9940b62332b2ce
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-18 10:57:55 -0700

    Added new comment functionality

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -9,25 +9,41 @@ class CommentSection extends React.Component {
         super();
         this.state = {
           comments: props.comments,
-          comment: ''
+          addComment: '',
+          time: ''
         }
         console.log(this.state.comments);
     }
-    addNewComment(event, index) {
-        event.preventDefault();
-
-
+    addNewComment = (comment) => {
+        const newComment = {
+            username: "kasia_bondarava",
+            text: comment
+        }
+        this.setState({comments: [...this.state.comments, newComment]});
     }
-
-    // const date = new Date();
-    // const time = moment(date).startOf('seconds').fromNow().toUpperCase();
+    onSubmit = (e) => {
+        e.preventDefault();
+        if(this.state.addComment !== "") {
+            this.addNewComment(this.state.addComment);
+        }
+        this.setState({addComment: ''});
+    }
+    onChange = (e)  => {
+        this.setState(
+            {[e.target.name]: e.target.value})
+    }
+    componentDidMount() {
+        const date = new Date();
+        const time = moment(date).startOf('seconds').fromNow().toUpperCase();
+        this.setState({time: time});
+    }
     render(props) {
         return (
             <div className="comment-section">
                 {this.state.comments.map((comment, index) => (<Comment key={index} comment={comment} />))}
-                {/* <p className="timestamp">{time}</p> */}
-                <form onSubmit={this.addNewComment}>
-                    <input className="add-comment-input" type="text" name="addComment" placeholder="Add a comment..." />
+                <p className="timestamp">{this.state.time}</p>
+                <form onSubmit={this.onSubmit}>
+                    <input className="add-comment-input" type="text" name="addComment" placeholder="Add a comment..." onChange={this.onChange} />
                 </form>
             </div>
         );
