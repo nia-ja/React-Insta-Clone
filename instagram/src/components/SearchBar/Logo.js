70c842df4db38ebdc61ec77b57149221342c7fb0
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-21 12:43:38 -0700

    Added styled-components for Logo component, deleted logo.css

--- instagram/src/components/SearchBar/Logo.js
+++ instagram/src/components/SearchBar/Logo.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import logoText from '../../Instagram_logo.svg';
-import './logo.css';
+import styled from 'styled-components';
+
+const LogoWrapper = styled.div `
+    min-width: 20%;
+    max-width: 40%;
+`;
+const AppLogo = styled.img `
+    max-height: 40px;
+    @media (max-width: 500px) {
+        ${props => (props.type === 'picture' ? `display: none` : null)}
+    }
+`;
 
 const Logo = () => {
     return (
-        <div className="logo">
-            <img src={logo} className="app-logo pic-logo" alt="Instagram logo" />
-            <img src={logoText} className="app-logo" alt="Instagram text logo" />
-        </div>
+        <LogoWrapper>
+            <AppLogo src={logo} type='picture' alt="Instagram logo" />
+            <AppLogo src={logoText} alt="Instagram text logo" />
+        </LogoWrapper>
     );
 }
 export default Logo;
